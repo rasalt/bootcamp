ac21c28e86f6e1c28be70f9f9e22338d3f77d005
root <root@ip-192-199-0-251.ec2.internal>
2017-08-18 13:01:42 -0400

    Latest files
    Signed-off-by: root <root@ip-192-199-0-251.ec2.internal>

---
+++ files/test.sh
@@ -0,0 +1,6 @@
+
+for time in {1..15000}
+do
+ echo time $time
+ curl 172.30.251.196:8080
+done
---
+++ pvs.sh
@@ -0,0 +1,61 @@
+
+#recycle does a scrub of the volume once the pod dies
+
+ssh master1.$GUID.internal
+sudo bash
+export GUID=`hostname|awk -F. '{print $2}'`
+echo $GUID
+
+export volsize="5Gi"
+mkdir /root/pvs
+for volume in pv{1..25} ; do
+cat << EOF > /root/pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "${volsize}"
+    },
+    "accessModes": [ "ReadWriteOnce" ],
+    "nfs": {
+        "path": "/srv/nfs/user-vols/${volume}",
+        "server": "support1.${GUID}.internal"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+echo "Created def file for ${volume}";
+done;
+
+
+# Dor a registry pod you would want the volume to be non reclaimable
+
+export volsize="10Gi"
+for volume in pv{26..50} ; do
+cat << EOF > /root/pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "${volsize}"
+    },
+    "accessModes": [ "ReadWriteMany" ],
+    "nfs": {
+        "path": "/srv/nfs/user-vols/${volume}",
+        "server": "support1.${GUID}.internal"
+    },
+    "persistentVolumeReclaimPolicy": "Retain"
+  }
+}
+EOF
+echo "Created def file for ${volume}";
+done;
